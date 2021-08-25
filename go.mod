6afc3cb84bf2ed6e2bf46abeb7c30d12ed8081a3
Laxman <laxman0202@gmail.com>
2021-08-24 20:08:14 -0400

    todo app

---
+++ go.mod
@@ -0,0 +1,32 @@
+module todo
+
+go 1.17
+
+require github.com/charmbracelet/bubbletea v0.14.1
+
+require (
+	github.com/BurntSushi/toml v0.3.1 // indirect
+	github.com/atotto/clipboard v0.1.2 // indirect
+	github.com/charmbracelet/bubbles v0.8.0 // indirect
+	github.com/charmbracelet/lipgloss v0.1.2 // indirect
+	github.com/containerd/console v1.0.1 // indirect
+	github.com/google/go-cmp v0.5.5 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/mattn/go-runewidth v0.0.12 // indirect
+	github.com/muesli/reflow v0.2.1-0.20210115123740-9e1d0d53df68 // indirect
+	github.com/muesli/termenv v0.8.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/sergi/go-diff v1.1.0 // indirect
+	golang.org/x/mod v0.4.2 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
+	golang.org/x/term v0.0.0-20210422114643-f5beecf764ed // indirect
+	golang.org/x/tools v0.1.6-0.20210802203754-9b21a8868e16 // indirect
+	golang.org/x/tools/gopls v0.7.1 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	honnef.co/go/tools v0.2.0 // indirect
+	mvdan.cc/gofumpt v0.1.1 // indirect
+	mvdan.cc/xurls/v2 v2.2.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,245 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+type Todo struct {
+	Text string `json:"text"`
+	Done bool   `json:"done"`
+}
+
+func (t Todo) String() string {
+	doneLabel := " "
+	if t.Done {
+		doneLabel = "x"
+	}
+	return fmt.Sprintf("[%s] %s", doneLabel, t.Text)
+}
+
+func (t *Todo) toggle() {
+	t.Done = !t.Done
+}
+
+func makeTodo(text string) Todo {
+	return Todo{Text: text, Done: false}
+}
+
+func getDataFilePath() (string, error) {
+	cacheDir, err := os.UserCacheDir()
+	if err != nil {
+		return "", fmt.Errorf("Could not load app data directory: %w\n", err)
+	}
+	if err = os.MkdirAll(filepath.Join(cacheDir, "todos"), os.ModePerm); err != nil {
+		return "", fmt.Errorf("Could not create todos directory: %w\n", err)
+	}
+	dataFilePath := filepath.Join(cacheDir, "todos", "data.json")
+	return dataFilePath, nil
+}
+
+func loadTodos() tea.Msg {
+	dataFilePath, err := getDataFilePath()
+	if err != nil {
+		return fmt.Errorf("Could not get data file path: %w\n", err)
+	}
+	if _, err = os.Stat(dataFilePath); os.IsNotExist(err) {
+		return todoLoadMsg([]Todo{})
+	}
+	dataFile, err := os.OpenFile(dataFilePath, os.O_RDONLY, os.ModePerm)
+	if err != nil {
+		return fmt.Errorf("Error opening file: %w\n", err)
+	}
+	defer dataFile.Close()
+	data, err := io.ReadAll(dataFile)
+	if err != nil {
+		return fmt.Errorf("Could not load data: %w\n", err)
+	}
+	newTodos := []Todo{}
+	err = json.Unmarshal(data, &newTodos)
+	if err != nil {
+		return fmt.Errorf("Invalid JSON read from file: %w\n", err)
+	}
+	return todoLoadMsg(newTodos)
+}
+
+func writeTodos(todos []Todo) tea.Cmd {
+	return func() tea.Msg {
+		bytes, err := json.Marshal(todos)
+		if err != nil {
+			return fmt.Errorf("Could not serialize todos: %w\n", err)
+		}
+		dataFilePath, err := getDataFilePath()
+		if err != nil {
+			return fmt.Errorf("Could not get data file path: %w\n", err)
+		}
+		err = os.WriteFile(dataFilePath, bytes, os.ModePerm)
+		if err != nil {
+			return fmt.Errorf("Could not write to file: %w\n", err)
+		}
+		return todoSaveMsg{}
+	}
+}
+
+type State int
+
+const (
+	NORMAL State = iota
+	APPEND
+)
+
+type model struct {
+	state     State
+	todos     []Todo
+	cursor    int
+	textInput textinput.Model
+	err       error
+}
+
+type todoLoadMsg []Todo
+type todoSaveMsg struct{}
+
+func (m *model) up() {
+	if m.cursor > 0 {
+		m.cursor--
+	}
+}
+
+func (m *model) down() {
+	if m.cursor < len(m.todos)-1 {
+		m.cursor++
+	}
+}
+
+func (m *model) gotoAdd() {
+	m.state = APPEND
+	m.textInput.Focus()
+}
+
+func (m *model) gotoNormal() {
+	m.state = NORMAL
+	m.textInput.Blur()
+	m.textInput.Reset()
+}
+
+func (m *model) toggle() {
+	m.todos[m.cursor].toggle()
+}
+
+func (m *model) addTodo(text string) {
+	m.todos = append(m.todos, makeTodo(text))
+}
+
+func (m *model) removeTodo() {
+	if len(m.todos) == 0 {
+		return
+	}
+	idx := m.cursor
+	m.todos = append(m.todos[:idx], m.todos[idx+1:]...)
+}
+
+func (m model) Init() tea.Cmd {
+	return loadTodos
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case todoLoadMsg:
+		m.todos = msg
+		ti := textinput.NewModel()
+		ti.CharLimit = 100
+		m.textInput = ti
+
+		return m, nil
+	case todoSaveMsg:
+		return m, tea.Quit
+	case error:
+		m.err = msg
+		return m, tea.Quit
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c":
+			return m, tea.Quit
+		}
+	}
+
+	if m.state == NORMAL {
+		switch msg := msg.(type) {
+		case tea.KeyMsg:
+			switch msg.String() {
+			case "j":
+				m.down()
+				return m, nil
+			case "k":
+				m.up()
+				return m, nil
+			case " ":
+				m.toggle()
+				return m, nil
+			case "A":
+				m.gotoAdd()
+				return m, nil
+			case "D":
+				m.removeTodo()
+				return m, nil
+			case "q":
+				return m, writeTodos(m.todos)
+			}
+		}
+	}
+
+	if m.state == APPEND {
+		switch msg := msg.(type) {
+		case tea.KeyMsg:
+			switch msg.Type {
+			case tea.KeyEsc:
+				m.gotoNormal()
+				return m, nil
+			case tea.KeyEnter:
+				text := m.textInput.Value()
+				if len(text) > 0 {
+					m.addTodo(text)
+					m.gotoNormal()
+				}
+				return m, nil
+			}
+		}
+		var cmd tea.Cmd
+		m.textInput, cmd = m.textInput.Update(msg)
+		return m, cmd
+	}
+	return m, nil
+}
+
+func (m model) View() string {
+	if m.err != nil {
+		return fmt.Sprintf("An error occurred: %v\n", m.err)
+	}
+	s := "Todos:\n"
+	if len(m.todos) == 0 {
+		s += "  No todos!\n"
+	}
+	for i, todo := range m.todos {
+		cursorTxt := " "
+		if m.cursor == i {
+			cursorTxt = "*"
+		}
+		s += fmt.Sprintf("  %s %s\n", cursorTxt, todo)
+	}
+	if m.state == APPEND {
+		s += m.textInput.View()
+	}
+	return s
+}
+
+func main() {
+	if err := tea.NewProgram(model{}).Start(); err != nil {
+		fmt.Printf("There was an error: %v\n", err)
+		os.Exit(1)
+	}
+}
